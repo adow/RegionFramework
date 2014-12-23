a1d09fbe0318ac017af53f01c85ad36d74604eda
adow <reynoldqin@gmail.com>
2014-12-23 10:57:02 +0800

    Initial Commit

---
+++ RegionFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RegionFramework
+//
+//  Created by 秦 道平 on 14/12/23.
+//  Copyright (c) 2014年 秦 道平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RegionFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RegionFramework
+//
+//  Created by 秦 道平 on 14/12/23.
+//  Copyright (c) 2014年 秦 道平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
